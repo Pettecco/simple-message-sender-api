ae3e1c16e1efe034daff3c521173806d4d6e1262
Pettecco <petterson.ikaro@gec.inatel.br>
2025-07-31 20:45:10 -0300

    feat: export users module to import service in messages module

--- src/message/message.module.ts
+++ src/message/message.module.ts
@@ -3,9 +3,10 @@ import { MessageService } from './message.service';
 import { MessageController } from './message.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Message } from './entities/message.entity';
+import { UsersModule } from 'src/users/users.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Message])],
+  imports: [TypeOrmModule.forFeature([Message]), UsersModule],
   providers: [MessageService],
   controllers: [MessageController],
 })

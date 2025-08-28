1598a68de951fa3b64cd290cc8844264126d510b
Pettecco <petterson.ikaro@gec.inatel.br>
2025-08-28 20:54:55 -0300

    feat: authenticate user with email and password using password hash and HashingService

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,9 +3,12 @@ import { HashingService } from './hashing/hashing.service';
 import { BcryptService } from './hashing/bcrypt.service';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from 'src/users/entities/user.entity';
 
 @Global()
 @Module({
+  imports: [TypeOrmModule.forFeature([User])],
   controllers: [AuthController],
   providers: [
     {
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,38 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { LoginDto } from './dto/login.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from 'src/users/entities/user.entity';
+import { HashingService } from './hashing/hashing.service';
 
 @Injectable()
 export class AuthService {
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+    private readonly hashingService: HashingService,
+  ) {}
+
   async login(loginDto: LoginDto) {
-    return loginDto;
+    const user = await this.userRepository.findOneBy({
+      email: loginDto.email,
+    });
+
+    if (!user) {
+      throw new UnauthorizedException('Esse usuário não existe');
+    }
+
+    const isPasswordValid = await this.hashingService.compare(
+      loginDto.password,
+      user.passwordHash,
+    );
+
+    if (!isPasswordValid) {
+      throw new UnauthorizedException('Senha inválida');
+    }
+
+    return {
+      message: 'Usuário logado!',
+    };
   }
 }

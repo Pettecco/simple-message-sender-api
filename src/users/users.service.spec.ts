bd81ff16190cd20927c775533fd97ac7f7a947fc
Pettecco <petterson.ikaro@gec.inatel.br>
2025-10-01 23:01:43 -0300

    test(nestjs): setup tests using TestingModule and Test from @nestjs/testing

---
+++ src/users/users.service.spec.ts
@@ -0,0 +1,36 @@
+import { Repository } from 'typeorm';
+import { HashingService } from 'src/auth/hashing/hashing.service';
+import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { UsersService } from './users.service';
+
+describe('PessoasService', () => {
+  let userService: UsersService;
+  let userRepository: Repository<User>;
+  let hashingService: HashingService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        UsersService,
+        {
+          provide: getRepositoryToken(User),
+          useValue: {},
+        },
+        {
+          provide: HashingService,
+          useValue: {},
+        },
+      ],
+    }).compile();
+
+    userService = module.get<UsersService>(UsersService);
+    userRepository = module.get<Repository<User>>(getRepositoryToken(User));
+    hashingService = module.get<HashingService>(HashingService);
+  });
+
+  it('pessoaService deve estar definido', () => {
+    expect(userService).toBeDefined();
+  });
+});

189fb1aeee82cf54b4e293f965bc9684a8355c5d
Pettecco <petterson.ikaro@gec.inatel.br>
2025-10-01 22:49:13 -0300

    feat(upload): add file validation using ParseFilePipe and ParseFilePipeBuilder

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -10,6 +10,9 @@ import {
   UseGuards,
   UseInterceptors,
   UploadedFile,
+  ParseFilePipeBuilder,
+  HttpStatus,
+  BadRequestException,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
@@ -62,15 +65,28 @@ export class UsersController {
   @UseInterceptors(FileInterceptor('file'))
   @Post('upload-picture')
   async uploadPicture(
-    @UploadedFile() file: Express.Multer.File,
+    @UploadedFile(
+      new ParseFilePipeBuilder()
+        .addFileTypeValidator({
+          fileType: /jpeg|jpg|npg/g,
+        })
+        .addMaxSizeValidator({
+          maxSize: 10 * (1024 * 1024),
+        })
+        .build({
+          errorHttpStatusCode: HttpStatus.UNPROCESSABLE_ENTITY,
+        }),
+    )
+    file: Express.Multer.File,
     @TokenPayloadParam() tokenPayload: TokenPayloadDto,
   ) {
+    if (file.size < 1024) {
+      throw new BadRequestException('File too small');
+    }
+
     const fileExtension = path
-
       .extname(file.originalname)
-
       .toLowerCase()
-
       .substring(1);
 
     const fileName = `${tokenPayload.sub}.${fileExtension}`;
